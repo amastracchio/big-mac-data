c2c63383fc81c9fec171cb3378094b46c665f182
ari <ari@yahoo.com>
2018-09-30 22:48:35 -0300

    Add graph

--- reg.pl
+++ reg.pl
@@ -2,10 +2,20 @@
 
 
 
+
 use strict;
 use warnings;
 use Data::Dumper;
 use Statistics::LineFit;
+use CGI; # not cgi but for use correct tags
+use GD::Graph;
+use List::Util qw(min max);
+use Chart::Gnuplot;
+
+
+# For make graph only
+my %slopes; 
+my %intercepts;
 
 my $lineFit = Statistics::LineFit->new();
 
@@ -24,10 +34,29 @@ my %paisgdp;
 my %base_currencies;
 my %base_currencies_adj;
 
-open (FILEGDP,"/root/bigmac/big-mac-data/source-data/big-mac-source-data.csv");
+
+my $q = CGI->new;
+
+# Solo para mostrar en html
+my $tmp_file_currencies = "/tmp/currencies.tmp";;
+open (FILEGDP,"/root/big-mac-data/big-mac-data/source-data/big-mac-source-data.csv") or die($!);
+open (HTML,">/root/3p/ht/htdocs/big-mac-index.html");
+open (TMP,">$tmp_file_currencies");
+print TMP "<table border=1 summary=\"The table show  the currency tables copyng from source data\"><tr><th>date</th><th>currency_code</th><th>dollar_price</th></tr>\n";
+print HTML "<HTML>";
 
 # primera linea la evitamos
 <FILEGDP>;
+
+
+print HTML $q->start_html( -title=>"Calculate big max indec from source and currencies data<",
+	                                -bgcolor=>"#ffffff");
+
+print HTML "<h1>Source data</h1>\n";
+print HTML "<strong>dollar_ex</strong> = price of the dollar in local currency. Precio de 1 dolar en moneda local<p>\n";
+print HTML "<strong>gdp</strong> = In dollars can be informed or not. Puede no estar informado<p>\n";
+print HTML "<strong>dollar_price</strong> = Price of big mac in dollars (local_price/dollar_ex). Calculated.<p>\n";
+
 while( <FILEGDP>){
 	# name,iso_a3,currency_code,local_price,dollar_ex,GDP_dollar,date
 	my @campos =  split(',');
@@ -59,17 +88,36 @@ while( <FILEGDP>){
 	}
 
 	$paisgdp{$date}{$iso_a3}{currency_code} = $currency_code;
+	$paisgdp{$date}{$iso_a3}{local_price} = $local_price;
+	$paisgdp{$date}{$iso_a3}{dollar_ex} = $dollar_ex;
 
 	if ($currency_code ~~ @basecurrencies) {
 
 		$base_currencies{$date}{$currency_code} = $dollar_price;
+		print TMP "<tr><td>$date</td><td>$currency_code</td><td>$dollar_price</td></tr>\n";
 	}
 	
 }
 
-print "Precios de big mac en 'base_currencies'\n";
-
 close FILEGDP;
+print HTML "<h1>Currencies table:</h1>\n";
+print HTML "<p>The currencies table is calculated (copied) from the source data below. Is the dollar price of big mac for each currencie. Not magic here.</b>";
+
+print TMP "</table>\n";
+close TMP;
+
+my $all;
+{
+	open (TMP, "<$tmp_file_currencies");
+	local $/ = undef;
+	$all = <TMP>;	
+	close TMP;
+}
+
+# Dump de las tablas de currencies bases
+print HTML $all;
+
+
 
 
 foreach  my $fecha (keys %paisgdp){
@@ -94,12 +142,14 @@ foreach  my $fecha (keys %paisgdp){
 
 				foreach my $cur (@basecurrencies) {
 
-					if (defined $base_currencies{fecha}{$cur}) {
+
+					if (defined $base_currencies{$fecha}{$cur}) {
+
 
 						# indice crudo raw
 						$index = $paisgdp{$fecha}{$iso_a3}{dollar_price} /$base_currencies{$fecha}{$cur} -1 ;
 						$index = sprintf("%.3f",$index);
-						# print "($cur) index raw= $index ";
+						# print   " ARI ($cur) index raw= $index ";
 
 						# guardamos el indice x currencie en hash
 						my $indexkey = $cur ."_raw";
@@ -127,6 +177,7 @@ foreach  my $fecha (keys %paisgdp){
 				}
 				#
 				print "\n";
+			# print HTML "<tr><td>$fecha</td><td>$iso_a3</td><td>".$paisgdp{$fecha}{$iso_a3}{currency_code}."</td><td>".$paisgdp{$fecha}{$iso_a3}{local_price}."</td><td>".$paisgdp{$fecha}{$iso_a3}{dollar_ex}."</td><td>".$paisgdp{$fecha}{$iso_a3}{dollar_price}."</td><td>".$paisgdp{$fecha}{$iso_a3}{gdp}."</th></tr>\n";
 
 
 		}
@@ -147,6 +198,9 @@ foreach  my $fecha (keys %paisgdp){
 		defined $intercept or die "Can't fit line if x values are all equal";
 		print "f(x) = $slope * x + $intercept\n";
 
+		# Lo guardamos para graficar nada mas..
+		$slopes{$fecha}{slope} = $slope;
+		$intercepts{$fecha}{intercept} = $intercept;
 
 	        
 		my @predictedYs = $lineFit->predictedYs();
@@ -177,7 +231,15 @@ foreach  my $fecha (keys %paisgdp){
 }
 
 
+print HTML "<table border=1 summary=\"The table shot the source input data currency code , local price, dollar exchange etc\"><tr><th>date</th><th>iso_a3</th><th>currency_code</th><th>local_price</th><th>dollar_ex</th><th>dollar_price (calculated)</th><th>gdp</th>";
 
+foreach my $cur (@basecurrencies) {
+	print HTML "<th> index ".$cur."_adjusted (calculated)</th>";
+	print HTML "<th> index ".$cur."_raw (calculated)</th>";
+}
+
+
+print HTML "</tr>";
 
 # Tenemos que calcular los indices ajustados por moneda. Recorrer de nuevo el hash
 
@@ -186,6 +248,7 @@ foreach  my $fecha (keys %paisgdp){
 
 		foreach my $iso_a3 (keys %{ $paisgdp{$fecha} }){
 
+			print "fecha = $fecha , iso_a3 = $iso_a3\n";
 			my $adj_price = $paisgdp{$fecha}{$iso_a3}{adj_price};
 
 			my $currency_code = $paisgdp{$fecha}{$iso_a3}{currency_code};
@@ -208,12 +271,38 @@ foreach  my $fecha (keys %paisgdp){
 
 # calculamos los adj_price segun las tablas de currencies
 foreach  my $fecha (keys %paisgdp){
+
+		# For make graph
+		my @xdata ;
+		my @ydata ;
+		my @ydata_adjusted ;
+
+		my $chart = Chart::Gnuplot->new(
+		       output  => "/root/3p/ht/htdocs/$fecha.png",
+		       xlabel => "GDP_dollar",
+		       title => "$fecha",
+		       ylabel => "Dollar_price"
+	         );
+
+
+
 		foreach my $iso_a3 (keys %{ $paisgdp{$fecha} }){
+
+			print HTML "<tr><td>$fecha</td><td>$iso_a3</td><td>".$paisgdp{$fecha}{$iso_a3}{currency_code}."</td><td>".$paisgdp{$fecha}{$iso_a3}{local_price}."</td><td>".$paisgdp{$fecha}{$iso_a3}{dollar_ex}."</td><td>".$paisgdp{$fecha}{$iso_a3}{dollar_price}."</td><td>".$paisgdp{$fecha}{$iso_a3}{gdp}."</td>";
+
+			if ( (defined $paisgdp{$fecha}{$iso_a3}{dollar_price}) and (defined $paisgdp{$fecha}{$iso_a3}{gdp}) ) {
+				push(@ydata,$paisgdp{$fecha}{$iso_a3}{dollar_price});
+				push(@xdata,$paisgdp{$fecha}{$iso_a3}{gdp});
+				push(@ydata_adjusted,$paisgdp{$fecha}{$iso_a3}{adj_price});
+		        }
+
 
 			foreach my $cur (@basecurrencies) {
 
 
-				  if ((defined $base_currencies_adj{$fecha}{$cur}) && (defined $paisgdp{$fecha}{$iso_a3}{adj_price})  ){
+				print HTML "<td>";
+
+			        if ((defined $base_currencies_adj{$fecha}{$cur}) && (defined $paisgdp{$fecha}{$iso_a3}{adj_price})  ){
 
 					# indice crudo raw
 					my $index = $paisgdp{$fecha}{$iso_a3}{adj_price} /$base_currencies_adj{$fecha}{$cur} -1 ;
@@ -223,10 +312,49 @@ foreach  my $fecha (keys %paisgdp){
 					# guardamos el indice x currencie en hash
 					my $indexkey = $cur ."_adjusted";
 					$paisgdp{$fecha}{$iso_a3}{$indexkey}  = $index ;
+					print HTML $index;
 
 				 }
+				print HTML "</td>";
+				print HTML "<td>";
+				# ARI
+				if ((defined $base_currencies{$fecha}{$cur}) && (defined $paisgdp{$fecha}{$iso_a3}{dollar_price}) ) {
+					my $indexkey = $cur ."_raw";
+					my $index = $paisgdp{$fecha}{$iso_a3}{$indexkey};
+					print HTML $index;
+				}
+				print HTML "</td>";
 			}
+			print HTML "</tr>";
+
+		} #foreach pais
+
+		my $dataSet;
+		my $dataSetadj;
+		if (scalar @xdata> 0 ){
+
+		   $dataSet = Chart::Gnuplot::DataSet->new(
+			        xdata => [@xdata],
+			        ydata => [@ydata],
+		 		style => "points",
+				title => "raw price"
+	           );
+
+		   $dataSetadj = Chart::Gnuplot::DataSet->new(
+			        xdata => [@xdata],
+			        ydata => [@ydata_adjusted],
+				title => "adjusted price",
+			        style => "linespoints"
+	           );
+
+
+		   $chart->plot2d($dataSet,$dataSetadj);
 		}
+
 }
 
+
+print HTML "</HTML>";
 die Dumper \%paisgdp;
+
+
